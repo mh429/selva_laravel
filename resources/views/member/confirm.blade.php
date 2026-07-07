405879ed7acdffad25637aa8c5acdcfe1a65a1d1
mh429 <sprout.hatanaka@gmail.com>
2026-07-07 15:48:31 +0900

    会員登録画面のCSSを実装

--- resources/views/member/confirm.blade.php
+++ resources/views/member/confirm.blade.php
@@ -1,34 +1,46 @@
 <x-layout>
 
-  <h1>会員情報確認画面</h1>
+  <div class="contents">
 
-  <table>
-    <tr>
-      <th>氏名</th>
-      <td>{{ $data['name_sei']. " ". $data['name_mei'] }}</td>
-    </tr>
-    <tr>
-      <th>ニックネーム</th>
-      <td>{{ $data['nickname'] }}</td>
-    </tr>
-    <tr>
-      <th>性別</th>
-      <td>{{ config('master.gender')[$data['gender']] }}</td>
-    </tr>
-    <tr>
-      <th>パスワード</th>
-      <td>セキュリティのため非表示</td>
-    </tr>
-    <tr>
-      <th>メールアドレス</th>
-      <td>{{ $data['email'] }}</td>
-    </tr>
-  </table>
+    <h1>会員情報確認画面</h1>
 
-  <form action="{{ route('member.store') }}" method="POST">
-    @csrf
-    <input type="submit" value="登録完了">
-  </form>
+    <div class="wrapper500">
 
-  <a href="{{ route('member.create') }}">前に戻る</a>
+      <table class="confirm_table">
+        <tr>
+          <th>氏名</th>
+          <td>{{ $data['name_sei']. " ". $data['name_mei'] }}</td>
+        </tr>
+        <tr>
+          <th>ニックネーム</th>
+          <td>{{ $data['nickname'] }}</td>
+        </tr>
+        <tr>
+          <th>性別</th>
+          <td>{{ config('master.gender')[$data['gender']] }}</td>
+        </tr>
+        <tr>
+          <th>パスワード</th>
+          <td>セキュリティのため非表示</td>
+        </tr>
+        <tr>
+          <th>メールアドレス</th>
+          <td>{{ $data['email'] }}</td>
+        </tr>
+      </table>
+
+      <form action="{{ route('member.store') }}" method="POST">
+        @csrf
+        <div class="div_tac">
+          <input type="submit" value="登録完了">
+        </div>
+      </form>
+
+    </div>
+
+    <div class="div_tac">
+      <a href="{{ route('member.create') }}" class="white_btn">前に戻る</a>
+    </div>
+  </div>
+
 </x-layout>
--- resources/views/member/store.blade.php
+++ resources/views/member/store.blade.php
@@ -1,9 +1,19 @@
 <x-layout>
 
-  <h1>会員登録完了</h1>
+  <div class="contents">
 
-  <p>会員登録が完了しました</p>
+    <h1>会員登録完了</h1>
 
-  <a href="/">トップに戻る</a>
-  
+    <div class="wrapper500">
+
+      <p class="p_tac">会員登録が完了しました。</p>
+
+    </div>
+
+    <div class="div_tac">
+      <a href="/" class="orange_btn">トップに戻る</a>
+    </div>
+
+  </div>
+
 </x-layout>
