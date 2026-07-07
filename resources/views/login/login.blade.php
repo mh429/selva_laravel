84a268087e0cbaefe0a5817653af29c472d87933
mh429 <sprout.hatanaka@gmail.com>
2026-07-07 16:39:18 +0900

    ログイン画面のCSSを実装

--- resources/views/login/login.blade.php
+++ resources/views/login/login.blade.php
@@ -1,29 +1,50 @@
 <x-layout>
 
-  <h1>ログイン</h1>
+  <div class="contents">
 
-  <form method="post">
-    @csrf
-    <p>
-        <label>メールアドレス（ID）</label>
-        <input type="text" name="email" value="{{ old('email') }}">
-    </p>
-    <p>
-        <label>パスワード</label>
-        <input type="password" name="password">
-    </p>
+    <h1>ログイン</h1>
 
-    <a href="{{ route('showPasswordResetMailForm') }}">パスワードを忘れた方はこちら</a>
+    <div class="wrapper500">
+      <form method="post">
+        @csrf
 
-    @if($errors->any())
-      <p style="color: red">{{ $errors->first() }}</p>
-    @endif
+        <div class="div_form_inputs">
+        <div>
+          <label class="input_wrapper">
+            <p>メールアドレス（ID）</p>
+            <input type="text" name="email" value="{{ old('email') }}" required class="input_250">              
+          </label>
+        </div>
+        <div>
+          <label class="input_wrapper">
+            <p>パスワード</p>
+            <input type="text" name="password" required class="input_250">              
+          </label>
+        </div>
 
-    <input type="submit" value="ログイン">
+        <div class="div_tac">
+          <a href="{{ route('showPasswordResetMailForm') }}" class="pass_forget">パスワードを忘れた方はこちら</a>  
+        </div>
 
-  </form>
+        <div class="error_wrapper">
+          @if($errors->any())
+            <p style="color: red">{{ $errors->first() }}</p>
+          @endif
+        </div>
 
-  <a href="/">トップに戻る</a>
+        </div>
 
+        <div class="div_tac">
+          <input type="submit" value="ログイン">
+        </div>
+
+      </form>
+    </div>
+
+    <div class="div_tac">
+      <a href="/" class="white_btn">トップに戻る</a>
+    </div>
+
+  </div>
 
 </x-layout>
--- resources/views/top.blade.php
+++ resources/views/top.blade.php
@@ -1,23 +1,24 @@
 <x-layout>
-  <header style="width: 800px; height:100px; background-color: #FBE4D5">
+  <header>
     @auth
       <p>{{ auth()->user()->name_sei }} {{ auth()->user()->name_mei }}様</p>
-      <div>
-        <a href="">商品一覧</a>
-        <a href="{{ route('product.create') }}">新規商品登録</a>
+      <div class="header_left">
+        <!-- <a href="">商品一覧</a> -->
+        <!-- <a href="{{ route('product.create') }}">新規商品登録</a> -->
         <form action="{{ route('logout') }}" method="POST">
           @csrf
-          <button type="submit">ログアウト</button>
+          <input type="submit" value="ログアウト">
         </form>            
       </div>
     @else
-      <div>
-        <a href="">商品一覧</a>
+      <p></p>
+      <div class="header_left">
+        <!-- <a href="">商品一覧</a> -->
         <a href="{{ route('member.create') }}">新規会員登録</a>   
         <a href="{{ route('login') }}">ログイン</a> 
       </div>
     @endauth
   </header>
 
-  <h1>TOP画面</h1>
+  
 </x-layout>
